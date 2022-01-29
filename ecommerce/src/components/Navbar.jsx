488c1d9ef2e93216e7ec33a1f0308565029f4b12
SJuras <jurasstpko@gmailcom>
2022-01-29 07:03:06 +0100

    v1.1.3 working on Announcement component.

--- ecommerce/src/components/Navbar.jsx
+++ ecommerce/src/components/Navbar.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import MenuIcon from '@material-ui/icons//Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import Badge from '@material-ui/core/Badge';
+import MailIcon from '@material-ui/icons/Mail';
+import ShoppingCartOutlined from '@material-ui/icons/ShoppingCartOutlined';
+
+
 const Container = styled.div`
   height: 60px;
 `
@@ -9,37 +16,87 @@ const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `
 
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+`
+
+const MobileMenuIcon = styled.div`
+  cursor: pointer;
+  margin-right: 18px;
 `
 
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  margin-top: -4px;
+`
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`
+
+const Input = styled.input`
+  border: none;
 `
 
 const Center = styled.div`
   flex: 1;
+  text-align: center;
+`
+
+const TitleLogo = styled.h2`
+  font-size: 36px;
 `
 
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
 `
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`
+
 
 const Navbar = () => {
   return(
     <Container>
       <Wrapper>
         <Left>
+          <MobileMenuIcon>
+            <MenuIcon style={{ fontSize: "36px"}} />
+          </MobileMenuIcon>
           <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <SearchIcon />
+          </SearchContainer>
         </Left>
         <Center>
-          center
+          <TitleLogo>neos.</TitleLogo>
         </Center>
         <Right>
-          right
+
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
         </Right>
       </Wrapper>
     </Container>
